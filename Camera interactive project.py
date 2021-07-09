5d457c7df24c0d21bda18429c83f34ad555a020f
Hardy <56915815+Funclex@users.noreply.github.com>
2021-07-09 14:01:55 +0100

    Camera interactive project
    功能： 通过摄像头捕捉黄的移动物体控制滚轮向下翻转
    扩展后可根据手势或者其他条件捕捉触发相应命令

---
+++ Camera interactive project.py
@@ -0,0 +1,41 @@
+"""
+---Camera interactive project---扶海滨 20210709---
+功能： 通过摄像头捕捉黄的移动物体控制滚轮向下翻转
+扩展后可根据手势或者其他条件捕捉触发相应命令
+Haibin FU – Bournemouth University
+hfu@bournemouth.ac.uk
+
+P.S。通过连接手机 webcam 远程控制
+"""
+
+import cv2
+import numpy as np
+import pyautogui
+
+cap = cv2.VideoCapture(1)
+
+yellow_lower = np.array([22, 93, 0])
+yellow_upper = np.array([45, 255, 255])
+prev_y = 0
+
+while True:
+    ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, yellow_lower, yellow_upper)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 300:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            if y < prev_y:
+                pyautogui.press('space')
+
+            prev_y = y
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
